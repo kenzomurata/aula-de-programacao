277c008863c34b5d268461740ed3104178643491
kenzomurata <kenzomurata@hotmail.com>
2022-04-07 19:56:45 -0300

    exercicio-da-aula-2

---
+++ aula-2/funcoes.js
@@ -0,0 +1,52 @@
+/*function comporFrase(nome, sobrenome, idade) {
+    var frase = "O indivíduo de nome " + nome + " " + sobrenome + " possui " + idade + " anos."
+    return frase
+}
+
+var fraseComposta = comporFrase("Fernando Kenzo", "Murata", 23)
+
+//window.alert(fraseComposta)
+
+function somar(numero1, numero2) {
+    var soma = numero1 + numero2
+    return soma
+}
+
+var operacao = somar(10, 5)
+
+//window.alert(operacao)
+
+function faseDaVida(idade) {
+    if(idade >= 18) {
+        return "É adulto"
+    } else if(idade >=12 && idade <18) {
+        return "É adolescente"
+    } else if(idade <12) {
+        return "É criança"
+    } 
+}
+
+var momentoDaVida = faseDaVida(11)
+
+var momentoDaVida2 = faseDaVida(19)
+
+//window.alert(momentoDaVida + " " + momentoDaVida2)
+*/
+function maiorNumero(numero1, numero2, numero3) {
+    if(numero1 ==numero2 ==numero3) {
+        return numero1
+    }
+    if(numero1 >numero2 && numero1 >numero3) {
+        return numero1
+    }
+    if(numero2 >numero1 && numero2 >numero3) {
+        return numero2
+    }
+    if(numero3 >numero1 && numero3 >numero2) {
+        return numero3
+    }
+}
+
+var numeros = maiorNumero(3, 31, 3)
+
+window.alert(numeros)
