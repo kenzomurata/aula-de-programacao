2f358d35d5bba1a3581d65428e977eac5290c8c7
kenzomurata <kenzomurata@hotmail.com>
2022-04-22 17:06:40 -0300

    exercicio

--- aula-3/exercicio-3.js
+++ aula-3/exercicio-3.js
@@ -1,3 +1,22 @@
-/*crie um loop que logue todos os numeros impares de 1 a 50
+/*crie um loop que encontre todos os números ímpares de 1 a 50
 o !(n%2 == 0)
 */
+
+
+function numerosImpares (contagem) {
+    if (contagem%2 != 0)
+        return true
+    else
+        return false
+}
+
+var contagem = 0
+
+
+do{
+    var eImpar = numerosImpares(contagem)
+    if (eImpar)
+        console.log (contagem)
+    contagem += 1
+} while(contagem < 51 )
+
