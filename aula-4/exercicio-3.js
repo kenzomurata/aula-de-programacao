275e93433b1068b782767d3ef4d03ec0efcb1bb5
kenzomurata <kenzomurata@hotmail.com>
2022-04-28 18:04:06 -0300

    exercicio

--- aula-4/exercicio-3.js
+++ aula-4/exercicio-3.js
@@ -1,1 +1,20 @@
-// funcção que receber Array de numeros e desvolve a media aritimetica
+// funcção que receber Array de numeros e devolve a media aritimetica
+
+var numeros = [10, 20, 30, 20]
+
+function mediaAritimetica(numeros){
+
+    var somatorio = 0
+    for(var indice = 0; indice < numeros.length; indice += 1){
+        somatorio += numeros[indice]
+    }
+    
+    var divisao = somatorio / numeros.length
+
+    return divisao
+
+}
+
+var resultado = mediaAritimetica(numeros)
+
+console.log(resultado)
