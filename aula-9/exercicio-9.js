f0866835aec4eee61a61b286fb4845724843b97a
kenzomurata <kenzomurata@hotmail.com>
2022-06-02 19:43:32 -0300

    aula 9

---
+++ aula-9/exercicio-9.js
@@ -0,0 +1,1 @@
+// array de imagem que vai ter titulo, imagem (endereço da imagem) e descrição e vao inserir no documento div contendo o titulo, a imagem e a descrição em um grid com colunas de 100px de largura de forma que o grid tente inserir o maximo de filhos em cada linha
