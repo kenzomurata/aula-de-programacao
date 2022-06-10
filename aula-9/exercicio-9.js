2c3a4b9d879d58a5d6cfbf409e34c7d1b6ee1ed6
kenzomurata <kenzomurata@hotmail.com>
2022-06-10 17:44:04 -0300

    exercicio 9

--- aula-9/exercicio-9.js
+++ aula-9/exercicio-9.js
@@ -1,1 +1,15 @@
 // array de imagem que vai ter titulo, imagem (endereço da imagem) e descrição e vao inserir no documento div contendo o titulo, a imagem e a descrição em um grid com colunas de 100px de largura de forma que o grid tente inserir o maximo de filhos em cada linha
+
+var imagens = [
+    {
+        titulo: "Cachorrinho",
+        imagem: "https://www.petfriendsworld.pt/wp-content/uploads/2020/04/husky-siberiano1.jpg",
+        descricao: "Filhote de cachorro",
+    },
+
+    {
+        titulo: "Gatinho",
+        imagem: ,
+        descricao: "Filhote de gato"
+    }
+]
