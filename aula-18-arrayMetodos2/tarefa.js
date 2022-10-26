f4ee5401a28877909d68567c1056eedabf6eb113
kenzomurata <kenzomurata@hotmail.com>
2022-10-26 18:59:15 -0300

    tarefa-18

--- aula-18-arrayMetodos2/tarefa.js
+++ aula-18-arrayMetodos2/tarefa.js
@@ -1,6 +1,30 @@
 /*
-1- Escreva uma função que receba uma palavra e uma letra e retorne um Array contendo as posições da palavra em que aparece a letra.
+1- Escreva uma função que receba uma palavra e uma letra e retorne um Array contendo as posições na palavra em que aparece a letra.
 Se a letra nao estiver na plavra, retorna um Array vazio.
 
 Obs.: a função deve desconsiderar acentos.
 */
+
+function separarPalavra (palavra, letra){
+
+    var letrasSeparadas = palavra.split("")
+
+    console.log(letrasSeparadas)
+
+    return letrasSeparadas
+  
+}
+
+function filtrarLetra (){
+
+    var posicapLetra = letrasSeparadas.filter(function(letra, indice){
+        
+    })
+
+}
+
+
+
+var posicaoLetra = letrasSeparadas.filter(function(letra, indice){
+    return letra[indice]
+})
